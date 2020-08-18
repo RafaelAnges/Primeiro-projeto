5843216832d7eb76d6c2dda70ea9af68fab5a01d
Rafael Santos <62733401+RafaelAnges@users.noreply.github.com>
2020-08-18 00:20:05 -0300

    Atualização +2

--- Site-1/js/main.js
+++ Site-1/js/main.js
@@ -1,1 +1,10 @@
-$('.slide-principal').slick();
+$('.slide-principal').slick({
+    dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  variableWidth: true
+  
+});
